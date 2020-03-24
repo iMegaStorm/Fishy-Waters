0101c5ae0cdb513f84440ac3ffeda14ac59ffb54
iMegaStorm <lukejkfinn@gmail.com>
2020-03-24 20:14:59 +0000

    Version 1.0.4
    Trying to figure out classes

--- CPP-SFMLTemplate/Fish.cpp
+++ CPP-SFMLTemplate/Fish.cpp
@@ -6,9 +6,25 @@
 //Compiler Directives
 using namespace std;
 
-Fish::Fish()
-{
+bool fishSpawn = false;
 
+Fish::Fish(sf::RenderWindow &window, sf::Sprite &water_4, sf::Sprite &sardine, sf::Sprite &trout, sf::Sprite &clownFish)
+{	
+}
+
+Fish::~Fish(void)
+{
+}
+
+void spawnFish(int fishType, int fishPositionX, int fishPositionY, sf::Sprite fishTypes[3], int startJ, int startI, sf::RenderWindow &window)
+{
+	sf::Sprite fish = fishTypes[fishType];
+	fish.setPosition((fishPositionX-startJ)*64, (fishPositionY-startI)*64);
+	window.draw(fish);
+}
+
+void fishSpawner(sf::RenderWindow &window, sf::Sprite &water_4, sf::Sprite &sardine, sf::Sprite &trout, sf::Sprite &clownFish)
+{
 	//Clock Variables
 	sf::Clock fishyClock;
 
@@ -21,8 +37,10 @@ Fish::Fish()
 	int fStartJ = 0;
 	
 	int fNoFishes = 0;
+	bool fishSpawn = true;
+	std::cout << fishSpawn << std::endl;
 
-	ifstream fishyMap("Assets/Map.txt"); //Reading map
+	sf::Sprite fishType[3] = {sardine, trout, clownFish};
 
 	int i, j;
 	srand(time(NULL));
@@ -30,7 +48,6 @@ Fish::Fish()
 	{
 		for(j=0; j < worldWidth; j++)
 		{
-			fishyMap>>fishyWorld[i][j];
 			if (fishyWorld[i][j] == 04)
 			{
 				int ransE = rand() %97; //random number of 20 * use a prime number 
@@ -40,43 +57,50 @@ Fish::Fish()
 				}
 			}
 		}
-		
 	}
+		int noFishes = 0;
+		for(i = fStartI; i < fStartI + 9; i++)
+		{
+			for(j = fStartJ; j < fStartJ + 13; j++)
+			{
+				if(fishyWorld[i][j] == 55 || fishyWorld[i][j] == 53 || fishyWorld[i][j] == 54)
+				{
+					water_4.setPosition((j-fStartJ)*64, (i-fStartI)*64);
+					window.draw(water_4);
+					spawnFish(fishyWorld[i][j] - 53,j,i, fishType, fStartJ, fStartI, window);
+					noFishes++;
+				} 
+			}
+		}
 
-					//getting the seconds
-					int sec = (int) fishyClock.getElapsedTime().asSeconds();
-					//add a row at every secToAdd seconds
-					int secToAdd = 15;
-					//adding a row only if the game started
-					srand(time(NULL));
-					if(sec % secToAdd == secToAdd - 1 && fNoFishes < 10)
-					{
-						int sI; //x co-ordinate
-						int sJ; //y co-ordinate
-						do 
-						{
-							sI = rand() % 9 + fStartI;  
-							sJ = rand() % 13 + fStartJ;
-						} 
-						while(fishyWorld[sI][sJ] != 4); //Checking the co-ordinates until it finds water
-						if(fishyWorld[i][j] >= 20)
-						{
-							int ransF = rand() %3; //3 potential fishes, random number to 3
-							if(ransF <= 2) //spawn a fish
-							{ //If the number is less than or equal to 2 then spawn in a fish
-								ransF += 53; //fish spawn starts at 53
-								fishyWorld[sI][sJ] = ransF; //spawn in a fish
-							}
-							fishyClock.restart();
-						}
-					}
-
-
-
-
+	//getting the seconds
+	int sec = (int) fishyClock.getElapsedTime().asSeconds();
+	//add a row at every secToAdd seconds
+	int secToAdd = 3;
+	//adding a row only if the game started
+	srand(time(NULL));
+	if(sec % secToAdd == secToAdd - 1 && fNoFishes < 10)
+	{
+		int sI; //x co-ordinate
+		int sJ; //y co-ordinate
+		do 
+		{
+			sI = rand() % 9 + fStartI;  
+			sJ = rand() % 13 + fStartJ;
+		} 
+		while(fishyWorld[sI][sJ] != 4); //Checking the co-ordinates until it finds water
+		if(fishyWorld[i][j] >= 20)
+		{
+			int ransF = rand() %3; //3 potential fishes, random number to 3
+			if(ransF <= 2) //spawn a fish
+			{ //If the number is less than or equal to 2 then spawn in a fish
+				ransF += 53; //fish spawn starts at 53
+				fishyWorld[sI][sJ] = ransF; //spawn in a fish
+			}
+			fishyClock.restart();
+			
+		}
+	}
 }
 
 
-Fish::~Fish(void)
-{
-}
--- CPP-SFMLTemplate/Fish.h
+++ CPP-SFMLTemplate/Fish.h
@@ -1,9 +1,10 @@
 #include "SFML\Graphics.hpp"
 #include <iostream>
 
-sf::Texture sardine;
-sf::Texture trout;
-sf::Texture clownfish;
+
+//sf::Texture sardine;
+//sf::Texture trout;
+//sf::Texture clownfish;
 
 
 
@@ -11,53 +12,57 @@ class Fish
 {
 public:
 	int value;
-	
 
-	Fish(void);
+
+
+public:
+	Fish(sf::RenderWindow &window, sf::Sprite &water_4, sf::Sprite &sardine, sf::Sprite &trout, sf::Sprite &clownFish);
 	~Fish(void);
+	void fishSpawner(sf::RenderWindow &window, sf::Sprite &water_4, sf::Sprite &sardine, sf::Sprite &trout, sf::Sprite &clownFish);
+	void spawnFish(sf::RenderWindow &window, int fishType, int fishPositionX, int fishPositionY, sf::Sprite fishTypes[3], int startJ, int startI);
 };
 
-class Sardine : public Fish
-{
-	public:
-
-		sf::Sprite sardine;
-		
-
-		Sardine()
-		{		
-			value = 20;
-			//fishText.loadFromFile("fishTile.png");
-		}
-};
-
-
-class Trout : public Fish
-{
-	public:
-
-		sf::Sprite trout;
-
-		/*Grouper()
-		{
-			value = 50;
-			grouperText.loadFromFile("GrouperTile.png");
-		}*/
-
-		//~Grouper();
-};
-
-class Clownfish : public Fish
-{
-	public:
-		
-		sf::Sprite clownfish;
-
-		/*clownfish()
-		{
-			value = 80;
-			clownfish.loadFromFile("barTile.png");
-		}*/
-
-
-};
+//class Sardine : public Fish
+//{
+//	public:
+//
+//		sf::Sprite sardine;
+//		
+//
+//		Sardine()
+//		{		
+//			value = 20;
+//			//fishText.loadFromFile("fishTile.png");
+//		}
+//};
+//
+//
+//class Trout : public Fish
+//{
+//	public:
+//
+//		sf::Sprite trout;
+//
+//		/*Grouper()
+//		{
+//			value = 50;
+//			grouperText.loadFromFile("GrouperTile.png");
+//		}*/
+//
+//		//~Grouper();
+//};
+//
+//class Clownfish : public Fish
+//{
+//	public:
+//		
+//		sf::Sprite clownfish;
+//
+//		/*clownfish()
+//		{
+//			value = 80;
+//			clownfish.loadFromFile("barTile.png");
+//		}*/
+//
+//
+//};
